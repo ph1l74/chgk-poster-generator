904b55ce3a8af62b40cc045ccab6642224e78d36
Filat Astakhov <astakhovfilat@gmail.com>
2019-11-26 14:40:34 +0300

    Добавлена функция смена режима, добавлена плавность изменения размера.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function imageUploadHandler(evt) {
-    const files = evt.target.files;
+function imageUploadHandler(event) {
+    const files = event.target.files;
     if (files.length > 1) {
         UIkit.notification('Выберите только один файл', { status: 'danger' });
         return false;
@@ -22,5 +22,53 @@ function imageUploadHandler(evt) {
     reader.readAsDataURL(files[0]);
 }
 
+function imageResizer(event) {
+    document.getElementById('imageContainer').style.height = `${event.target.value}px`
+}
 
-document.getElementById('fileUploader').addEventListener('change', imageUploadHandler, false);
+function displayNone(elem) {
+    elem.style.display = 'none';
+}
+
+function hideElem(id) {
+    const elem = document.getElementById(id);
+    elem.classList.remove('animated', 'fadeInDown', 'faster');
+    elem.classList.add('animated', 'fadeOutDown', 'faster');
+    elem.addEventListener('animationend', displayNone(elem));
+}
+
+function showElem(id) {
+    const elem = document.getElementById(id);
+    elem.removeEventListener('animationend', displayNone(elem));
+    elem.style.display = 'block';
+    elem.classList.remove('animated', 'fadeOutDown', 'faster');
+    elem.classList.add('animated', 'fadeInDown', 'faster');
+}
+
+function changeCoverMode(event) {
+    let selectedMode = event.target.value;
+    if (selectedMode === 'gradient') {
+        hideElem('uploadOptions');
+        showElem('gradientOptions');
+    }
+    else {
+        showElem('uploadOptions');
+        hideElem('gradientOptions');
+    }
+}
+
+function init() {
+    document.getElementsByName('sizeselector').forEach(function () {
+        addEventListener('change', imageResizer, false);
+    });
+
+    document.getElementsByName('covermode').forEach(function () {
+        addEventListener('change', changeCoverMode, false);
+    });
+
+    document.getElementById('fileUploader').addEventListener('change', imageUploadHandler, false);
+
+    hideElem('gradientOptions');
+
+}
+
